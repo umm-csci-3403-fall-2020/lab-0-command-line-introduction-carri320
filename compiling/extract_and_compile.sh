b629c16f3c36585d34580de549c54d1e84004a0f
carri320 <carri320@morris.umn.edu>
2020-08-30 19:45:22 -0500

    Created big_clean.sh and made it executable

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -4,3 +4,5 @@
 tar -zxf NthPrime.tgz #-z uncompress the files, -x extracts the files, -f tells tar that a file name will be given
 cd NthPrime #Goes into the NthPrime directory 
 gcc main.c nth_prime.c nth_prime.h -o NthPrime #gcc compiles the programs, -o tells it to put the output in the NthPrime directory  
+NUMBER=$1 #Creates a varibale
+./NthPrime "$NUMBER" #Runs NthPrime taking the variable NUMBER (should be a number but a letter will result in a 0)
